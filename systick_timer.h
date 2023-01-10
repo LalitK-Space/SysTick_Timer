b25f8de376300bf8b2630d22179c609a9323d671
Lalit Kumar <115681964+LalitK-Space@users.noreply.github.com>
2023-01-09 22:43:18 -0600

    Update systick_timer.h

--- systick_timer.h
+++ systick_timer.h
@@ -13,7 +13,7 @@
 #define SYST_CSR		(volatile uint32_t *) 0xE000E010	// SysTick Control and Status Register
 
 /* -- Clock Source for SysTick Timer -- */
-#define SYSTICK_TIMER_CLOCK		16000000U				// SysTick CLock Source (set to 16MHz)
+#define SYSTICK_TIMER_CLOCK		16000000U				        // SysTick CLock Source (set to 16MHz)
 
 
 /* -- API to configure the SysTick Timer to generate a desired exception time interval --  */
